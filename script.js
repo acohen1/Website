9fb290c21a4267749f6791b5dd418862cc5749d7
Alex Cohen <alexcohen4080@gmail.com>
2024-12-11 06:25:00 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('greetBtn').addEventListener('click', function () {
+    const greeting = document.getElementById('greeting');
+    greeting.textContent = 'Hello, World! Welcome to my site!';
+  });
+  
